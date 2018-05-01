9a73b3f21a938864464a5fcd57a475ed99a24fdd
Shawn Wildermuth <shawn@wildermuth.com>
2018-05-01 15:34:25 -0400

    Beyond Bower Webcast

---
+++ PS-BeyondBower/Examples/yarn/SimpleWebApp/gulpfile.js
@@ -0,0 +1,42 @@
+﻿var gulp = require("gulp");
+var rimraf = require("rimraf");
+var merge = require('merge-stream'); 
+
+// Dependency Dirs
+var deps = {
+  "jquery": {
+    "dist/*": "dist"
+  },
+  "bootstrap": {
+    "dist/**/*": "dist"
+  },
+  "jquery-validation": {
+    "dist/*": "dist"
+  },
+  "jquery-validation-unobtrusive": {
+    "dist/*": "dist"
+  },
+
+};
+
+gulp.task("clean", function (cb) {
+  return rimraf("wwwroot/lib/", cb);
+});
+
+gulp.task("scripts", function () {
+
+  var streams = [];
+
+  for (var prop in deps) {
+    console.log("Prepping Scripts for: " + prop);
+    for (var itemProp in deps[prop]) {
+      streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+        .pipe(gulp.dest("wwwroot/lib/" + prop + "/" + deps[prop][itemProp])));
+    }
+  }
+
+  return merge(streams);
+
+});
+
+gulp.task("default", ['scripts']);
