868dc085c4a1a680cfc75a77b468fd1d830d832f
Shawn Wildermuth <shawn@wildermuth.com>
2018-10-04 14:02:30 +0200

    Techorama Beyond Bower

---
+++ Techorama/2018/BeyondBower/Examples/CoreYarn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreYarn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
