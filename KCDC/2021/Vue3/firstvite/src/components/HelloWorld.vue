118cd73bbdd70142482cedb8ad7fc36d2f0f8b61
Shawn Wildermuth <shawn@wildermuth.com>
2021-09-16 16:53:41 -0500

    KCDC Vue 3 Demo

---
+++ KCDC/2021/Vue3/firstvite/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+TT<template>
+<div>Heloo</div>
+<Teleport to='#ourMenu'>
+  <div @click="onClick">{{ uppercase(name) }}</div>
+</Teleport>
+</template>
+
+<script lang="ts">
+import { ref, computed, reactive, defineComponent } from "vue";
+//import filters from "../filters";
+
+export default defineComponent({
+  props: {
+    id: {
+      required: true
+    }
+  },
+  setup(props, opt) {
+
+    opt.
+
+   const name = ref("Shawn");
+   const items = reactive([]);
+    function onClick() {
+      name.value = "Bob";
+    }
+
+   const nameLength = computed(() => name.value.length);
+
+   function uppercase(val: String) {
+     return val.toUpperCase();
+   } 
+     return {
+     name,
+     nameLength,
+     items,
+     onClick,
+     uppercase,
+//     ...filters
+    }
+  }
+});
+</script>
+
+
